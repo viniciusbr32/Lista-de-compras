1121c7cdfb6a203ff8799d8ab1c8664c0d6d199b
viniciusbr32 <viniciusgba2008@gmail.com>
2024-08-01 16:18:44 -0300

    input Checked

--- lista-de-compras/src/components/cards/cards.tsx
+++ lista-de-compras/src/components/cards/cards.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 interface CardsProps {
   text: string;
   handleClickDelete: (id: string) => void;
@@ -5,12 +7,17 @@ interface CardsProps {
 }
 
 export function Cards({ text, handleClickDelete, idTodelete }: CardsProps) {
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(event.target.checked);
+  };
+
   return (
     <div className="pt-10 space-y-5">
       <div className="flex items-center justify-between px-4 py-4 bg-white h-13 rounded-xl">
-        <div className="flex items-center gap-2">
-          <input type="checkbox" className="w-4 h-4" />
-
+        <div className={`flex items-center gap-2 ${isChecked && 'line-through text-gray-400'} `}>
+          <input id="checkbox" onChange={handleCheckboxChange} checked={isChecked} type="checkbox" />
           <p className="font-medium font-Inter">{text}</p>
         </div>
         <button onClick={() => handleClickDelete(idTodelete)}>
